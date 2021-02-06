d18989d334a0fe5a2bf9f1678a6d329f65280717
gf808080 <kekruse@gmail.com>
2021-02-06 18:14:29 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,80 @@
+import dash
+import pandas as pd
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SUPERHERO])
+
+app.layout = html.Div([
+dbc.Row([
+        html.H1("Search a number and set precision threshold"),
+            ], justify="center", align="center"
+    ),
+    html.Br(),
+    dbc.Row(
+        dbc.Col(
+            html.Div(
+                dcc.Slider(
+                    id='slider_threshold',
+                    min=0,
+                    max=52,
+                    step=1,
+                    marks={"1": "Precision 1",
+                           "12": "Precision 12",
+                           "24": "Precision 24",
+                           "36": "Precision 36",
+                           "48": "Precision 48"},
+                    value=24,
+                ),
+            ),
+        )
+    ),
+    dbc.Row(
+        [dbc.Col(children=[
+             dbc.Input(id="id_no", type="string"),
+             html.H4("ID_No"),
+                ]),
+        dbc.Col(
+            dbc.Button("submit_number", id="search-button"),
+            )
+        ]
+    ),
+
+    html.Div(id='container-button-basic')
+     ])
+@app.callback(
+    [Output('slider_threshold', 'value'),
+    Output('container-button-basic','children')],
+    [Input("search-button", 'n_clicks'),
+    Input('slider_threshold', 'value')],
+    State('id_no', 'value')
+)
+
+def make_table(n_clicks, slid_val, id_no):
+    if n_clicks is not None:
+        print(slid_val, id_no)
+        base = "https://imgur.com/gallery/{}"
+        frame = pd.read_csv('/home/sentinel/PycharmProjects/pythonProject/separated.csv')
+        titles = []
+        links = []
+        for i, row in frame.iterrows():
+            titles.append(row['key'])
+            hl= base.format(row['item'])
+            links.append(html.A(html.P('Link'), href=hl))
+        dictionary={"title":titles,"link":links}
+        linkframe = pd.DataFrame(dictionary)
+        fullframe = pd.merge(frame, linkframe, left_on='key', right_on='title')
+        filterframe = fullframe[fullframe['score']>=int(slid_val)]
+        outframe = filterframe[['key','link', 'item', 'text' ]]
+        table=dbc.Table.from_dataframe(outframe, striped=True, bordered=True, hover=True)
+        return slid_val, table
+
+        n_clicks == None
+    else:
+        return(24, None)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
